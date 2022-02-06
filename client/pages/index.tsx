0184c5e7f32a83bbe6ff98f7461e514c730581d3
Second24Life <maka123456789@mail.ru>
2022-02-07 05:19:59 +0700

    install material ui

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
+// import {Button} from "@material-ui/core";
+// import Navbar from "../components/Navbar";
+// import MainLayout from "../layouts/MainLayout";
 
-type Props = {};
-
-const Index = (props: Props) => {
-  return <div>Главная страница</div>;
+const Index = () => {
+  return (
+    <>
+      <Navbar>
+        <div className='center'>
+          <h1>Добро пожаловать!</h1>
+          <h3>Здесь собраны лучшие треки!</h3>
+        </div>
+      </Navbar>
+    </>
+  );
 };
 
 export default Index;
